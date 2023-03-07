ccee668ea4722c72c62f6e530e28f3b88f4dad02
Dimitri Pierre-Louis <dimitripl6@techexchange.in>
2023-03-07 04:17:44 +0000

    Added functionality

--- flaskr/__init__.py
+++ flaskr/__init__.py
@@ -5,13 +5,16 @@ from flask import Flask
 import logging
 logging.basicConfig(level=logging.DEBUG)
 
+from flask_login import LoginManager
+
 # The flask terminal command inside "run-flask.sh" searches for
 # this method inside of __init__.py (containing flaskr module 
 # properties) as we set "FLASK_APP=flaskr" before running "flask".
 def create_app(test_config=None):
     # Create and configure the app.
     app = Flask(__name__, instance_relative_config=True)
-
+    login_manager = LoginManager()
+    
     # This is the default secret key used for login sessions
     # By default the dev environment uses the key 'dev'
     app.config.from_mapping(
@@ -29,4 +32,5 @@ def create_app(test_config=None):
     # TODO(Project 1): Make additional modifications here for logging in, backends
     # and additional endpoints.
     pages.make_endpoints(app)
+    login_manager.init_app(app)
     return app
