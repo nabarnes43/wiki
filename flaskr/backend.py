471b60dc15705630276c85c57697790e28e37b0e
nabarnes43 <nabarnes43@techexchange.in>
2023-04-11 04:25:49 +0000

    check_page_author and test + upload changes

--- flaskr/backend.py
+++ flaskr/backend.py
@@ -1,6 +1,7 @@
 # TODO(Project 1): Implement Backend according to the requirements.
 from google.cloud import storage
 from google.cloud import exceptions
+from flask_login import current_user
 import hashlib
 import io
 from flask import Flask
@@ -21,21 +22,20 @@ class Backend:
             The contents of the wiki page.
 
         Raises:
-            exceptions.NotFound: If the specified wiki page is not found.
             Exception: If there is a network error.
         """
+        bucket = self.storage_client.bucket("sdswiki_contents")
+        blob = bucket.get_blob(name)
+        if blob is None:
+            return f"Error: Wiki page {name} not found."
+
         try:
-            bucket = self.storage_client.bucket("sdswiki_contents")
-            blob = bucket.blob(name)
             with blob.open() as f:
                 content = f.read()
             return content
-
-        except exceptions.NotFound:
-            return f"Error: Wiki page {name} not found."
-
         except Exception as e:
             return f"Network error: {e}"
+
 
     def get_all_page_names(self):
         """Gets the names of all wiki pages.
@@ -61,7 +61,8 @@ class Backend:
         except Exception as e:
             return f"Network error: {e}"
 
-    def upload(self, data, destination_blob_name):
+    #Need to upload some meta data a long with upload    
+    def upload(self, data, destination_blob_name, username):
         '''
         Uploads page to Wiki server
 
@@ -73,6 +74,7 @@ class Backend:
             A response message stating if your upload was successful or not.
             If the upload was unsuccessulf, the reason why would be displayed.
         '''
+        
         if data == b'':
             return 'Please upload a file.'
         if destination_blob_name == '':
@@ -85,14 +87,19 @@ class Backend:
             if destination_blob_name == blob.name:
                 return 'Upload failed. You cannot overrite an existing page'
 
+
         blob = bucket.blob(destination_blob_name)
+
+        # Set the x-goog-meta-author metadata header
+        blob.metadata = {'author': username}
+
         blob.upload_from_string(data)
 
         return f"{destination_blob_name} uploaded to Wiki."
 
     def sign_up(self, name, password):
         '''
-        Allows a person to create an account on the wiki if they are using it for th efirst time.
+        Allows a person to create an account on the wiki if they are using it for the first time.
 
         Args:
             name = This acts as the username of the person
@@ -163,3 +170,49 @@ class Backend:
             img = f.read()
 
         return img
+
+    def check_page_author(self, name):
+        """
+        Retrieves the author metadata of a blob with the given name from the Google Cloud Storage bucket.
+
+        Args:
+            name (str): The name of the blob to retrieve the author metadata from.
+
+        Returns:
+        If the specified blob exists and has an author metadata, returns the author's name as a string.
+        If the specified blob does not exist or does not have an author metadata, returns None.
+        If an error occurs while retrieving the metadata, returns None and prints an error message.
+        """
+
+        try:
+            bucket = self.storage_client.bucket("sdswiki_contents")
+            blob = bucket.get_blob(name)
+            if blob:
+                try:
+                    metadata = blob.metadata
+                    author = metadata.get('author')
+                    if author is not None:
+                        print(f"The author of the blob is {author}")
+                        return author
+                    else:
+                        print(f"The blob does not have an author metadata.")
+                        return None
+                        
+                except AttributeError:
+                    print(f"The specified blob does not have any metadata.")
+                    return None
+                        
+                except Exception as e:
+                    print(f"An error occurred while trying to retrieve the metadata: {e}")
+                    return None
+            else:
+                print(f"The specified blob does not exist.")
+                return None
+
+        except exceptions.NotFound:
+            return f"Error: Wiki page {name} not found."
+
+        except Exception as e:
+            return f"Network error: {e}"
+
+
